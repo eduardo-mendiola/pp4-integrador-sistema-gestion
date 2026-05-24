058f1a6c54ed5d849f7ddba2f72e34a08715eb69
Eduardo E. Mendiola <93398008+eduardo-mendiola@users.noreply.github.com>
2026-05-24 16:14:39 -0300

    feat: completar RoleController con métodos CRUD y crear roles.http

--- src/routes/role-routes.js
+++ src/routes/role-routes.js
@@ -6,7 +6,12 @@ const router = express.Router();
 
 router.use(isAuthenticated);
 
+
 router.get('/', RoleController.getAll);
 router.get('/:id', RoleController.getById);
+router.post('/', RoleController.create);
+router.patch('/:id', RoleController.partialUpdate);
+router.put('/:id', RoleController.update);
+router.delete('/:id', RoleController.remove);
 
 export default router;
