b0ec934fd74dfc92b53f82dbf6abf84dec682595
Eduardo E. Mendiola <93398008+eduardo-mendiola@users.noreply.github.com>
2026-06-16 10:42:44 -0300

    fix: listen on 0.0.0.0 for Render

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ import "dotenv/config";
 import app from "./src/app.js";
 import dbConnect from "./src/config/db.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 (async () => {
   try {
     await dbConnect();
     console.log("MongoDB connected");
 
-    app.listen(PORT, "127.0.0.1", () => {
-      console.log(`API server running on http://127.0.0.1:${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`API server running on ${PORT}`);
     });
   } catch (error) {
     console.error("DB connection error:", error);
